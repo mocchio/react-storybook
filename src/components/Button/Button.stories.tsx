10529ff118fa70cd4d42a68201dfa3efca9acc6a
Takumi Sakamoto <124575646+mocchio@users.noreply.github.com>
2024-12-03 18:00:51 +0900

    feat: clickイベントとactionsの設定

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -19,6 +19,9 @@ const meta = {
         type: "select",
       },
     },
+    handleClick: {
+      action: "clicked",
+    },
   },
 } satisfies Meta<typeof Button>;
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,20 @@ type Props = {
   children: React.ReactNode;
   color?: string;
   size?: string;
+  handleClick?: () => void;
 };
 
-function Button({ children, color = "default", size = "base" }: Props) {
-  return <button className={`${color} ${size}`}>{children}</button>;
+function Button({
+  children,
+  color = "default",
+  size = "base",
+  handleClick = () => console.log("click"),
+}: Props) {
+  return (
+    <button className={`${color} ${size}`} onClick={handleClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
