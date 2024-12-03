99984592c21d897858f9f91f812a33216023cc31
Takumi Sakamoto <124575646+mocchio@users.noreply.github.com>
2024-12-03 18:18:00 +0900

    feat: データフェッチ処理を追加

--- src/components/UserItem/UserItem.stories.tsx
+++ src/components/UserItem/UserItem.stories.tsx
@@ -15,3 +15,18 @@ type Story = StoryObj<typeof UserItem>;
 export const Default: Story = {
   args: { id: "1", name: "Alice" },
 };
+
+export const FetchData: Story = {
+  loaders: [
+    async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await response.json();
+      return { user: data };
+    },
+  ],
+  render: (args, { loaded: { user } }) => (
+    <UserItem {...args} id={user.id} name={user.name} />
+  ),
+};
