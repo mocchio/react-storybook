738539b81c61d72d495b70958aa6cdf06ede961c
Takumi Sakamoto <124575646+mocchio@users.noreply.github.com>
2024-12-03 18:13:09 +0900

    feat: UserItemコンポーネントを追加

---
+++ src/components/UserItem/UserItem.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import UserItem from "./UserList";
+
+const meta = {
+  title: "UserItem",
+  component: UserItem,
+  tags: ["autodocs"],
+} satisfies Meta<typeof UserItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof UserItem>;
+
+export const Default: Story = {
+  args: { id: "1", name: "Alice" },
+};
